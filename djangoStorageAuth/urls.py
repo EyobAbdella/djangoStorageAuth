16f79f8156ce1a90eed60f3903d340c07a354072
Eyob Abdella <eyobabdellasharo@gmail.com>
2025-04-30 17:21:07 -0300

    Add Microsoft OAuth redirect and callback

--- djangoStorageAuth/urls.py
+++ djangoStorageAuth/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path("google/redirect", views.GoogleRedirectViewSet.as_view({"get": "list"})),
     path("google/callback", views.GoogleCallbackViewSet.as_view({"get": "list"})),
+    path("microsoft/redirect", views.MicrosoftRedirectViewSet.as_view({"get": "list"})),
+    path("microsoft/callback/", views.MicrosoftCallbackViewSet.as_view({"get": "list"})),
+
 ]
--- djangoStorageAuth/views.py
+++ djangoStorageAuth/views.py
@@ -1,4 +1,5 @@
 
+from datetime import datetime, timedelta
 from random import SystemRandom
 from urllib.parse import urlencode
 import jwt
@@ -103,6 +104,115 @@ class GoogleCallbackViewSet(viewsets.ModelViewSet):
         oauth_tokens, _ = OAuthTokens.objects.get_or_create(user=user)
         oauth_tokens.google_access = access_token
         oauth_tokens.google_refresh = refresh_token 
+
+        user.save()
+
+        refresh = RefreshToken.for_user(user)
+        token = TokenObtainPairSerializer().get_token(user)
+
+        return Response({
+            "access_token": str(token.access_token),
+            "refresh_token": str(refresh),
+        }, status=status.HTTP_200_OK)
+
+
+
+
+
+
+class MicrosoftRedirectViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.none()
+    serializer_class = UserSerializer
+
+    def list(self, request, *args, **kwargs):
+        SCOPES = [
+            "User.Read",
+            "Files.ReadWrite",
+        ]
+
+        rand = SystemRandom()
+        state = "".join(rand.choice(UNICODE_ASCII_CHARACTER_SET) for _ in range(30))
+        cache.set(state, True, timeout=500)
+
+        redirect_uri = request.build_absolute_uri("/oauth/microsoft/callback/")
+        redirect_uri = redirect_uri.replace("127.0.0.1", "localhost")
+        params = {
+            "client_id": settings.MICROSOFT_CLIENT_ID,
+            "response_type": "code",
+            "redirect_uri": redirect_uri,
+            "scope": " ".join(SCOPES),
+            "state": state,
+            "response_mode": "query",
+        }
+
+        authorization_url = f"https://login.microsoftonline.com/common/oauth2/v2.0/authorize?{urlencode(params)}"
+        return redirect(authorization_url)
+
+class MicrosoftCallbackViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def list(self, request, *args, **kwargs):
+        code = request.GET.get("code")
+        state = request.GET.get("state")
+        error = request.GET.get("error")
+
+        if error:
+            return Response({"error": error}, status=status.HTTP_400_BAD_REQUEST)
+
+        if not code or not state:
+            return Response({"error": "code and state are required."}, status=status.HTTP_400_BAD_REQUEST)
+
+        if not cache.get(state):
+            return Response({"error": "Invalid or expired state."}, status=status.HTTP_400_BAD_REQUEST)
+
+        cache.delete(state)
+
+        token_url = "https://login.microsoftonline.com/common/oauth2/v2.0/token"
+        redirect_uri = request.build_absolute_uri("/oauth/microsoft/callback/")
+        redirect_uri = redirect_uri.replace("127.0.0.1", "localhost")
+        data = {
+            "client_id": settings.MICROSOFT_CLIENT_ID,
+            "client_secret": settings.MICROSOFT_CLIENT_SECRET,
+            "code": code,
+            "grant_type": "authorization_code",
+            "redirect_uri": redirect_uri,
+        }
+
+        response = requests.post(token_url, data=data)
+        if not response.ok:
+           return Response({"error": "Failed to exchange code for token"}, status=status.HTTP_400_BAD_REQUEST)
+
+        tokens = response.json()
+        access_token = tokens.get("access_token")
+        refresh_token = tokens.get("refresh_token")
+        
+        expires_in = tokens.get("expires_in")
+        expiration_time = datetime.utcnow() + timedelta(seconds=expires_in)
+
+
+        if not access_token:
+            return Response({"error": "Access token missing"}, status=status.HTTP_400_BAD_REQUEST)
+
+        user_info_url = "https://graph.microsoft.com/v1.0/me"
+        headers = {"Authorization": f"Bearer {access_token}"}
+        user_info_response = requests.get(user_info_url, headers=headers)
+
+        if not user_info_response.ok:
+            return Response({"error": "Failed to fetch user info from Microsoft"}, status=status.HTTP_400_BAD_REQUEST)
+
+        user_info = user_info_response.json()
+        email = user_info.get("userPrincipalName")
+
+        if not email:
+            return Response({"error": "Email not found in Microsoft user info"}, status=status.HTTP_400_BAD_REQUEST)
+
+        user, _ = User.objects.get_or_create(email=email, username=email)
+        login(request, user)
+        oauth_tokens, _ = OAuthTokens.objects.get_or_create(user=user)
+        oauth_tokens.microsoft_access = access_token
+        oauth_tokens.microsoft_refresh = refresh_token 
+        oauth_tokens.microsoft_expiry = expiration_time 
 
         user.save()
 
